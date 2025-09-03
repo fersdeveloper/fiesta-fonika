7a87e194c852ee82347f8bd533487ed3b2c9ea30
Fernando Tirado <fersdeveloper@gmail.com>
2025-09-02 20:29:40 -0600

    Primera versión

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+});
